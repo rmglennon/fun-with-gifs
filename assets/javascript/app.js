cf86936ea3cb1a84c38725f6fceb3bab2c11aa2a
Rhonda Glennon <rhondaglennon@gmail.com>
2018-01-30 23:21:22 -0800

    get buttons showing on page, add html divs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,47 @@
-var topics = ["cookie", "cupcake", "banana", "cheese", "pizza", "tofu", "pie", "lettuce", "ice cream", "salad", "apple", "strawberry", "burrito", "soup", "taco", "watermelon", "chocolate", "pasta", "water", "pineapple"];
+$(document).ready(function(){
 
-for (var i = 0; i < topics.length; i++) {
-  var newButton = $("<button>");
-  newButton.text(topics[i]);
-  newButton.addClass("buttons")
+createButtons();
+
+
+
+function createButtons() {
+
+  var topics = ["cookie", "cupcake", "banana", "cheese", "pizza", "tofu", "pie", "lettuce", "ice cream", "salad", "apple", "strawberry", "burrito", "soup", "taco", "watermelon", "chocolate", "mashed potatoes", "pasta", "water"];
+
+  for (var i = 0; i < topics.length; i++) {
+    var newButton = $("<button>");
+    newButton.text(topics[i]);
+    newButton.addClass("buttons");
+    newButton.attr("data-topic", topics[i]);
+    newButton.attr("id", topics[i]);
+    $(".gif-buttons").append(newButton);
+  }
 }
 
-// var queryURL = "https://api.giphy.com/v1/gifs/trending?";
-// var api_key = "2Djlw2Z3UfS0QNM9FkTSOO3bh0o3KOoc"
-//
-//
-// $.ajax({
-//   url: queryURL,
-//   method: 'GET'
-// }).then(function(response) {
-//   console.log(response);
-//
-//
-// });
+$(".buttons").on("click", function() {
+  // base URL for search endpoint (q=)
+  var baseURL = "https://api.giphy.com/v1/gifs/search?q=";
+  var apiKey = "api_key=2Djlw2Z3UfS0QNM9FkTSOO3bh0o3KOoc";
+  var searchTerm = $(this).text();
+  var limit = 10; //default is 25
+  var rating;
+
+  var queryURL = baseURL + searchTerm + "&" + "limit="+ limit + "&" + apiKey;
+    console.log("search term: " + searchTerm);
+
+  $.ajax({
+    url: queryURL,
+    method: 'GET'
+  }).then(function(response) {
+    console.log(response);
+    var image = ("<img>");
+    var imageURL = response[0].images.fixed_height_still.url;
+    image.attr("src", imageURL);
+    //image.attr("alt", "")
+    $("#gif-container").append(image);
+
+  });
+
+});
+
+});
